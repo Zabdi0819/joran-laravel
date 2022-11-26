a10312afd01ff004b095265f13b089f19d454e52
Zabdi <zabdibvb19@gmail.com>
2022-11-25 18:25:24 -0600

    owl carousel-Pt16

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -20,6 +20,9 @@
     <!-- Styles -->
     <link href="{{ asset('frontend/css/custom.css') }}" rel="stylesheet">
     <link href="{{ asset('frontend/css/bootstrap5.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('frontend/css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('frontend/css/owl.theme.default.min.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -29,7 +32,9 @@
         @yield('content')
     </div>
 
+    <script src="{{ asset('frontend/js/jquery-3.6.1.min.js') }}"></script>
     <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
     
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     @if(session('status'))
