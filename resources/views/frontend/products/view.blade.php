854e97e56f2762ed59f4711c179aa702fef62bac
Zabdi <zabdibvb19@gmail.com>
2022-11-26 18:40:34 -0600

    QtyIncrementDecrement-Pt21

--- resources/views/frontend/products/view.blade.php
+++ resources/views/frontend/products/view.blade.php
@@ -36,15 +36,15 @@
                             <label class="badge bg-danger">Sin productos disponibles</label>
                         @endif
                         <div class="row mt-2">
-                            <div class="col-md-2">
+                            <div class="col-md-3">
                                 <label for="Quantity">Cantidad</label>
-                                <div class="input-group text-center mb-3">
-                                    <span class="input-group-text">-</span>
-                                    <input type="text" name="quantity" value="1" class="form-control"/>
-                                    <span class="input-group-text">+</span>
+                                <div class="input-group text-center mb-3" style="width: 130px;">
+                                    <button class="input-group-text decrement-btn">-</button>
+                                    <input type="text" name="quantity" value="1" class="form-control text-center qty-input"/>
+                                    <button class="input-group-text increment-btn">+</button>
                                 </div>
                             </div>
-                            <div class="col-md-10">
+                            <div class="col-md-9">
                                 <br/>
                                 <button type="button" class="btn btn-success me-3 float-start">Agregar a favoritos  <i class="fas fa-heart"></i></button>
                                 <button type="button" class="btn btn-primary me-3 float-start">Agregar al carrito <i class="fa fa-shopping-cart"></i></button>
@@ -55,4 +55,34 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function(){
+            $('.increment-btn').click(function(e){
+                e.preventDefault();
+                var inc_value = $('.qty-input').val();
+                var value = parseInt(inc_value, 10);
+                value = isNaN(value) ? 0 : value;
+                if(value < 10)
+                {
+                    value++;
+                    $('.qty-input').val(value)
+                }
+            });
+
+            $('.decrement-btn').click(function(e){
+                e.preventDefault();
+                var dec_value = $('.qty-input').val();
+                var value = parseInt(dec_value, 10);
+                value = isNaN(value) ? 0 : value;
+                if(value > 1)
+                {
+                    value--;
+                    $('.qty-input').val(value)
+                }
+            });
+        });
+    </script>
 @endsection
